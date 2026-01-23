decad388e814710547eced7480d91050a5c12385
Joseph Kowalczyk <jmkowalczyk@wisc.edu>
2026-01-23 11:26:40 -0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Script by Joseph Kowalczyk, 2026
+    var thirdline=document.getElementById("thirdLine");
+    var counter = 0
+    console.log(thirdline)
+    thirdline.addEventListener("click", function(){
+        alert("Hello World!");
+    })
